e0274b9f971596166d42db1565f8839474c62651
Piotr <piotrurban1996@gmail.com>
2022-03-05 21:28:20 +0100

    Zadanie 9.2

--- src/com/company/devices/Phone.java
+++ src/com/company/devices/Phone.java
@@ -2,8 +2,14 @@ package com.company.devices;
 
 import com.company.devices.Device;
 
+import java.util.List;
+
 public class Phone extends Device {
     Double screenSize;
+    final static Double appVersion = 2.0;
+    final static String serverIP = "94.23.90.4";
+
+
 
     @Override
     public void turnOn(){
@@ -16,4 +22,21 @@ public class Phone extends Device {
     public String toString(){
         return model+" "+yearOfProduction+" "+producer;
     }
+    public void installAnnApp(String appName){
+
+    }
+    public void installAnnApp(String appName, Double appVersion){
+
+    }
+    public void installAnnApp(String appName, Double appVersion, String serverIP){
+
+    }
+    public void installAnnApp(List<String> appsNames){
+
+    }
+    public void installAnnApp(Object url,String appName, Double appVersion, String serverIP){
+
+    }
+
+
 }
