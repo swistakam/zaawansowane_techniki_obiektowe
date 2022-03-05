e803dacf14abb15f46bf8e7c449b8a0ea4fc8ec3
Piotr <piotrurban1996@gmail.com>
2022-03-05 21:04:35 +0100

    Zadanie 9

---
+++ src/com/company/creatures/FarmAnima.java
@@ -0,0 +1,14 @@
+package com.company.creatures;
+
+import com.company.creatures.Animal;
+
+public class FarmAnima extends Animal {
+    FarmAnima(String species, String name) {
+        super(species, name);
+    }
+
+    @Override
+    public void beEaten(){
+
+    }
+}
---
+++ src/com/company/creatures/Pet.java
@@ -0,0 +1,14 @@
+package com.company.creatures;
+
+import com.company.creatures.Animal;
+
+public class Pet extends Animal {
+    public Pet(String species, String name) {
+        super(species, name);
+    }
+
+    @Override
+    public void beEaten() {
+
+    }
+}
